4561a91bc9eb671600510877b387e823f74e6a28
Kevin Wang <b09855121@gmail.com>
2016-12-17 11:55:30 +0800

    Add files via upload

---
+++ StepperMotor.ino
@@ -0,0 +1,45 @@
+
+/*-----( Import needed libraries )-----*/
+#include <Stepper.h>
+
+/*-----( Declare Constants, Pin Numbers )-----*/
+//---( Number of steps per revolution of INTERNAL motor in 4-step mode )---
+#define STEPS_PER_MOTOR_REVOLUTION 32   
+
+//---( Steps per OUTPUT SHAFT of gear reduction )---
+#define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048  
+ 
+
+/*-----( Declare objects )-----*/
+// create an instance of the stepper class, specifying
+// the number of steps of the motor and the pins it's
+// attached to
+
+//The pin connections need to be 4 pins connected
+// to Motor Driver In1, In2, In3, In4  and then the pins entered
+// here in the sequence 1-3-2-4 for proper sequencing
+Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
+
+/*-----( Declare Variables )-----*/
+int  Steps2Take;
+
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+// Nothing  (Stepper Library sets pins as outputs)
+}/*--(end setup )---*/
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  Steps2Take  =  STEPS_PER_OUTPUT_REVOLUTION ;  // Rotate CW 1 turn
+  small_stepper.setSpeed(500);   
+  small_stepper.step(Steps2Take);
+  delay(300);
+  
+  Steps2Take  =  - STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CCW 1 turn  
+  small_stepper.setSpeed(500);  // 700 a good max speed??
+  small_stepper.step(Steps2Take);
+  delay(500);
+
+}/* --(end main loop )-- */
+
+/* ( THE END ) */
